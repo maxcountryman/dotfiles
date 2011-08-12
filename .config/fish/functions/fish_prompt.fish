01659e617f9d8e2c730043880a707425fe3c7925
Max Countryman <maxc@me.com>
2011-08-12 07:17:58 -0400

    updating

--- .config/fish/functions/fish_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_prompt -d "Write out the prompt"
-        printf '%s[%s%s%s@%s%s%s:%s%s%s%s]%s> ' (set_color blue) (set_color normal) (whoami) (set_color blue) (set_color normal) (hostname|cut -d . -f 1) (set_color blue) (set_color normal) (set_color $fish_color_cwd) (prompt_pwd) (set_color blue) (set_color normal)
-end
